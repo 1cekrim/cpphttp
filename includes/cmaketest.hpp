fe38734445fe2cf19fd51373eff591abf4994b8d
1cekrim <2019027192@hanyang.ac.kr>
2020-05-01 06:11:12 +0900

    doctest 테스츠

---
+++ includes/cmaketest.hpp
@@ -0,0 +1,8 @@
+#ifndef CPPHTTP_CMAKETEST_H
+#define CPPHTTP_CMAKETEST_H
+
+namespace cpphttp
+{
+bool cmaketest();
+}  // namespace cpphttp
+#endif  // CPPHTTP_CMAKETEST_H
---
+++ sources/cmaketest.cc
@@ -0,0 +1,9 @@
+#include <cmaketest.hpp>
+
+namespace cpphttp
+{
+bool cmaketest()
+{
+    return true;
+}
+}  // namespace cpphttp
