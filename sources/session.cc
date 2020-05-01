433f4a99ec939125728c20cb56a2d024b6c64c42
1cekrim <1cekrim.dev@gmail.com>
2020-05-02 03:38:09 +0900

    response 클래스 추가

--- sources/session.cc
+++ sources/session.cc
@@ -4,5 +4,13 @@ namespace cpphttp
 {
 Session::Session() : headers {}
 {
+    // Do nothing
+}
+
+Response::Response(const std::string_view& text, int status_code)
+    : text(text),
+      status_code(status_code)
+{
+    // Do nothing
 }
 }  // namespace cpphttp
---
+++ tests/session_test.cc
@@ -0,0 +1,11 @@
+#include <doctest.h>
+
+#include <iostream>
+#include <session.hpp>
+
+using namespace cpphttp;
+
+TEST_CASE("session")
+{
+    Session sess;
+}
