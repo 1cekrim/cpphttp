73d45c30cbac6b65593bac9dfdd547fd57bc45ad
1cekrim <1cekrim.dev@gmail.com>
2020-05-02 00:25:50 +0900

    headers 구현

--- includes/headers.hpp
+++ includes/headers.hpp
@@ -4,9 +4,16 @@
 #include <iosfwd>
 #include <string>
 #include <string_view>
+#include <vector>
 
 namespace cpphttp
 {
+enum class Method
+{
+    GET,
+    POST
+};
+
 struct Header
 {
     Header(const std::string_view& name, const std::string_view& value);
@@ -15,6 +22,24 @@ struct Header
     friend std::ostream& operator<<(std::ostream& os, const Header& header);
 
     std::string name, value;
+};
+
+class Headers
+{
+ public:
+    friend std::ostream& operator<<(std::ostream& os, const Header& header);
+    Headers();
+    Headers(std::initializer_list<Header> il);
+    void set_method(Method m);
+    void set_url(const std::string_view& url);
+    const std::vector<Header>& get_headers() const;
+    std::string to_string() const;
+
+    friend std::ostream& operator<<(std::ostream& os, const Headers& headers);
+
+ private:
+    std::vector<Header> headers;
+    std::string method, url;
 };
 }  // namespace cpphttp
 
--- tests/headers_test.cc
+++ tests/headers_test.cc
@@ -10,3 +10,13 @@ TEST_CASE("header")
     Header header { "Connection", "keep-alive" };
     std::cout << header;
 }
+
+TEST_CASE("headers_init")
+{
+    Headers headers { { "Host", "github.com" },
+                      { "Connection", "keep-alive" } };
+    headers.set_method(Method::GET);
+    headers.set_url("http://github.com");
+
+    std::cout << headers;
+}
